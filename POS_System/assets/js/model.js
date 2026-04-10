caf9ab3b7c59a97ea4f5b930a24acb60c07698de
Thushal Eranda <madush9989@gmail.com>
2026-04-10 22:05:39 +0530

    created use array object and ininitialize other array objects in model.js

---
+++ POS_System/assets/js/model.js
@@ -0,0 +1,23 @@
+const MODEL = {
+    users: [{
+        userName:"admin",
+        password: "admin1234",
+    }],
+
+    // Current User
+    currentUser: null,
+
+    // Customer Array
+    customers: [],
+
+    // Product Array
+    items: [],
+
+    // Order Array
+    orders: [],
+
+    // All Objects Auto ID's
+    customerID: 1,
+    productID: 1,
+    orderID: 1,
+}
