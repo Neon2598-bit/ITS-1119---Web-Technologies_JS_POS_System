2dba5d4acadbc66cad5944b66eacaa99aaed22cf
Thushal Eranda <madush9989@gmail.com>
2026-04-11 19:07:22 +0530

    implement VIEW module with SPA section transitions and login UI helpers

--- POS_System/assets/js/view.js
+++ POS_System/assets/js/view.js
@@ -1,12 +1,30 @@
 const VIEW = {
 
     showSection: function (sectionID) {
-        $('section').hide();
-        $('#' + sectionID).show();
+        const $CURRENT = $('section.active');
+        const $NEXT = $('#'+sectionID);
+
+        if ($CURRENT.length) {
+            $CURRENT.css('opacity', '0');
+
+            setTimeout(() => {
+                $CURRENT.removeClass('active');
+
+                $NEXT.addClass('active').css('opacity', '0');
+
+                requestAnimationFrame(() => {
+                    requestAnimationFrame(() => {
+                        $NEXT.css('opacity', '1');
+                    });
+                });
+            }, 400);
+        } else {
+            $NEXT.addClass('active').css('opacity', '1');
+        }
     },
 
     showLoginError: function (message) {
-        $('#loginError').text(message).show();
+        $('#loginError').text(message).fadeIn(200);
     },
 
     hideLoginError: function () {
@@ -16,5 +34,9 @@ const VIEW = {
     clearLoginForm: function () {
         $('#emailInput').val('');
         $('#passwordInput').val('');
+    },
+
+    setWelcomeName: function (name) {
+        $('#welcomeName').text(name);
     }
 }
